08f3a770c646a570b10e08b5becffe86c97f3249
Sahil Gohil <38555487+sahilgohil@users.noreply.github.com>
2023-06-14 10:34:02 +1000

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,15 @@
+ static class Edge{
+        int sc;
+        int nbr;
+        int wt;
+
+        public Edge(int sc, int nbr, int wt)
+        {
+            this.sc = sc;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+
+
+
+    }
